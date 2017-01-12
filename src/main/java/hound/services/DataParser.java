237938ffc10eca7bd3a27791981f7d0863ca1616
diogomdo <diogomdo@gmail.com>
2017-01-12 17:25:41 +0000

    Parsing Json data with success.

--- src/main/java/hound/services/DataParser.java
+++ src/main/java/hound/services/DataParser.java
@@ -1,9 +1,12 @@
 package hound.services;
 
+import java.util.Arrays;
 import java.util.List;
 
+import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import hound.model.WeatherSpotInfo;
 
@@ -11,10 +14,17 @@ public class DataParser {
 
 	public static List<WeatherSpotInfo> getParsedData(Document weatherPageResponse) {
 		List<WeatherSpotInfo> data = null;
-		weatherPageResponse.toString();
-		
-		Element table = weatherPageResponse.select("table[class=tabulka]").first();
-		table.toString();
+
+		Elements scriptElements = weatherPageResponse.getElementsByTag("script");
+		for (Element element : scriptElements) {
+			for (DataNode dataNode : element.dataNodes()) {
+				if (dataNode.toString().startsWith("var wg_fcst_tab_data_1")) {
+					List<String> jsonObjList = Arrays.asList(dataNode.getWholeData().split(";"));
+					String weatherDataString = jsonObjList.get(0).replaceFirst("^[^{]+", "");
+					System.out.println(weatherDataString);
+				}
+			}
+		}
 		return data;
 	}
 
